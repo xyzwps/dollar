1c227f9afcba34e09f6eb9977d60968334b8b1b0
亮玉 韦 <weily@yingmi.cn>
2023-04-20 14:29:37 +0800

    pad

--- src/main/java/com/xyzwps/lib/dollar/Dollar.java
+++ src/main/java/com/xyzwps/lib/dollar/Dollar.java
@@ -302,6 +302,17 @@ public final class Dollar {
 
 
         /**
+         * Check if the string is empty or not.
+         *
+         * @param string to be checked
+         * @return true if string is null or it's length is 0
+         */
+        public static boolean isEmpty(String string) {
+            return string == null || string.isEmpty();
+        }
+
+
+        /**
          * Check value which is falsey or not. The values <code>null</code>, <code>false</code>,
          * <code>0(.0)</code> and <code>""</code> are falsey.
          *
@@ -329,6 +340,18 @@ public final class Dollar {
 
 
         /**
+         * Check if the string is not empty.
+         *
+         * @param string to be checked
+         * @param <T>    list element type
+         * @return true if string {@link #isEmpty(String)} is false
+         */
+        public static <T> boolean isNotEmpty(String string) {
+            return !isEmpty(string);
+        }
+
+
+        /**
          * Create a list.
          *
          * @param elements elements of list
@@ -338,6 +361,100 @@ public final class Dollar {
         @SafeVarargs
         public static <T> List<T> list(T... elements) {
             return Arrays.asList(elements);
+        }
+
+
+        /**
+         * Pads <code>string</code> on the left and right sides if it's shorter than <code>length</code>.
+         * Padding characters are truncated if they can't be evenly divided by <code>length</code>.
+         *
+         * @param string The string to pad
+         * @param length The padding length
+         * @param chars  The string used as padding
+         * @return Padded string
+         */
+        public static String pad(String string, int length, String chars) {
+            if (length < 0) {
+                throw new IllegalArgumentException("Argument length cannot be less than 0");
+            }
+
+            string = defaultTo(string, "");
+            if (string.length() >= length) {
+                return string;
+            }
+
+            char[] padChars = (isEmpty(chars) ? " " : chars).toCharArray();
+            StringBuilder sb = new StringBuilder();
+            int padLength = length - string.length();
+            int padHalf = padLength / 2;
+            for (int i = 0; i < padHalf; i++) {
+                sb.append(padChars[i % padChars.length]);
+            }
+            sb.append(string);
+            for (int i = padHalf; i < padLength; i++) {
+                sb.append(padChars[i % padChars.length]);
+            }
+            return sb.toString();
+
+        }
+
+
+        /**
+         * Pads <code>string</code> on the right side if it's shorter than <code>length</code>.
+         * Padding characters are truncated if they exceed <code>length</code>.
+         *
+         * @param string The string to pad
+         * @param length The padding length
+         * @param chars  The string used as padding
+         * @return Padded string
+         */
+        public static String padEnd(String string, int length, String chars) {
+            if (length < 0) {
+                throw new IllegalArgumentException("Argument length cannot be less than 0");
+            }
+
+            string = defaultTo(string, "");
+            if (string.length() >= length) {
+                return string;
+            }
+
+            char[] padChars = (isEmpty(chars) ? " " : chars).toCharArray();
+            StringBuilder sb = new StringBuilder(string);
+            int padLength = length - string.length();
+            for (int i = 0; i < padLength; i++) {
+                sb.append(padChars[i % padChars.length]);
+            }
+            return sb.toString();
+        }
+
+
+        /**
+         * Pads <code>string</code> on the left side if it's shorter than <code>length</code>.
+         * Padding characters are truncated if they exceed <code>length</code>.
+         *
+         * @param string The string to pad
+         * @param length The padding length
+         * @param chars  The string used as padding
+         * @return Padded string
+         */
+        public static String padStart(String string, int length, String chars) {
+            if (length < 0) {
+                throw new IllegalArgumentException("Argument length cannot be less than 0");
+            }
+
+            string = defaultTo(string, "");
+            if (string.length() >= length) {
+                return string;
+            }
+
+            char[] padChars = (isEmpty(chars) ? " " : chars).toCharArray();
+            StringBuilder sb = new StringBuilder();
+            int padLength = length - string.length();
+            for (int i = 0; i < padLength; i++) {
+                sb.append(padChars[i % padChars.length]);
+            }
+            sb.append(string);
+            return sb.toString();
         }
 
 
--- src/test/java/com/xyzwps/lib/dollar/DollarTests.java
+++ src/test/java/com/xyzwps/lib/dollar/DollarTests.java
@@ -72,4 +72,88 @@ class DollarTests {
         assertEquals("[1, 2, 3, 4]", $.concat($.list(1, 2), $.list(3, 4)).toString());
         assertEquals("[1, 2, 3, 4]", $.concat($.list(1, 2), null, $.list(3, 4)).toString());
     }
+
+    @Test
+    void pad() {
+        assertThrows(IllegalArgumentException.class, () -> $.pad("", -1, " "));
+
+        assertEquals("      ", $.pad(null, 6, null));
+        assertEquals("      ", $.pad(null, 6, ""));
+        assertEquals("      ", $.pad(null, 6, " "));
+        assertEquals("aaaaaa", $.pad(null, 6, "a"));
+        assertEquals("ababab", $.pad(null, 6, "ab"));
+        assertEquals("abcdab", $.pad(null, 6, "abcd"));
+
+        assertEquals("      ", $.pad("", 6, null));
+        assertEquals("      ", $.pad("", 6, ""));
+        assertEquals("      ", $.pad("", 6, " "));
+        assertEquals("aaaaaa", $.pad("", 6, "a"));
+        assertEquals("ababab", $.pad("", 6, "ab"));
+        assertEquals("abcdab", $.pad("", 6, "abcd"));
+
+        assertEquals(" +++  ", $.pad("+++", 6, null));
+        assertEquals(" +++  ", $.pad("+++", 6, ""));
+        assertEquals(" +++  ", $.pad("+++", 6, " "));
+        assertEquals("a+++aa", $.pad("+++", 6, "a"));
+        assertEquals("a+++ba", $.pad("+++", 6, "ab"));
+        assertEquals("a+++bc", $.pad("+++", 6, "abcd"));
+
+        assertEquals("+++---***", $.pad("+++---***", 6, "abcd"));
+    }
+
+    @Test
+    void padEnd() {
+        assertThrows(IllegalArgumentException.class, () -> $.padEnd("", -1, " "));
+
+        assertEquals("      ", $.padEnd(null, 6, null));
+        assertEquals("      ", $.padEnd(null, 6, ""));
+        assertEquals("      ", $.padEnd(null, 6, " "));
+        assertEquals("aaaaaa", $.padEnd(null, 6, "a"));
+        assertEquals("ababab", $.padEnd(null, 6, "ab"));
+        assertEquals("abcdab", $.padEnd(null, 6, "abcd"));
+
+        assertEquals("      ", $.padEnd("", 6, null));
+        assertEquals("      ", $.padEnd("", 6, ""));
+        assertEquals("      ", $.padEnd("", 6, " "));
+        assertEquals("aaaaaa", $.padEnd("", 6, "a"));
+        assertEquals("ababab", $.padEnd("", 6, "ab"));
+        assertEquals("abcdab", $.padEnd("", 6, "abcd"));
+
+        assertEquals("+++   ", $.padEnd("+++", 6, null));
+        assertEquals("+++   ", $.padEnd("+++", 6, ""));
+        assertEquals("+++   ", $.padEnd("+++", 6, " "));
+        assertEquals("+++aaa", $.padEnd("+++", 6, "a"));
+        assertEquals("+++aba", $.padEnd("+++", 6, "ab"));
+        assertEquals("+++abc", $.padEnd("+++", 6, "abcd"));
+
+        assertEquals("+++---***", $.padEnd("+++---***", 6, "abcd"));
+    }
+
+    @Test
+    void padStart() {
+        assertThrows(IllegalArgumentException.class, () -> $.padStart("", -1, " "));
+
+        assertEquals("      ", $.padStart(null, 6, null));
+        assertEquals("      ", $.padStart(null, 6, ""));
+        assertEquals("      ", $.padStart(null, 6, " "));
+        assertEquals("aaaaaa", $.padStart(null, 6, "a"));
+        assertEquals("ababab", $.padStart(null, 6, "ab"));
+        assertEquals("abcdab", $.padStart(null, 6, "abcd"));
+
+        assertEquals("      ", $.padStart("", 6, null));
+        assertEquals("      ", $.padStart("", 6, ""));
+        assertEquals("      ", $.padStart("", 6, " "));
+        assertEquals("aaaaaa", $.padStart("", 6, "a"));
+        assertEquals("ababab", $.padStart("", 6, "ab"));
+        assertEquals("abcdab", $.padStart("", 6, "abcd"));
+
+        assertEquals("   +++", $.padStart("+++", 6, null));
+        assertEquals("   +++", $.padStart("+++", 6, ""));
+        assertEquals("   +++", $.padStart("+++", 6, " "));
+        assertEquals("aaa+++", $.padStart("+++", 6, "a"));
+        assertEquals("aba+++", $.padStart("+++", 6, "ab"));
+        assertEquals("abc+++", $.padStart("+++", 6, "abcd"));
+
+        assertEquals("+++---***", $.padStart("+++---***", 6, "abcd"));
+    }
 }
