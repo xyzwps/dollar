b0226b06bf87c765b9ad23856349cdd1defb8745
亮玉 韦 <weily@yingmi.cn>
2023-05-05 10:15:37 +0800

    doc Unreachable

--- src/main/java/com/xyzwps/lib/dollar/Unreachable.java
+++ src/main/java/com/xyzwps/lib/dollar/Unreachable.java
@@ -1,4 +1,14 @@
 package com.xyzwps.lib.dollar;
 
+/**
+ * Throw when executing a unreachable branch.
+ */
 public class Unreachable extends RuntimeException {
+
+    /**
+     * Default constructor is ok.
+     */
+    public Unreachable() {
+        super("The branch is unreachable. Maybe a BUG.");
+    }
 }
